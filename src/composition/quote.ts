f493e79e48b4ab3784d95190ffd178e9e2cc288b
Bopp <importantbopp@gmail.com>
2021-05-29 15:10:28 +0200

    update backend ip

--- src/composition/quote.ts
+++ src/composition/quote.ts
@@ -1,6 +1,6 @@
 export default function useQuote() {
   const getQuote = async () => {
-    const data = await fetch('http://34.102.191.29/quotes/random', {
+    const data = await fetch('http://34.102.134.122/quotes/random', {
       method: 'GET',
     })
     const { text }: { index: string; text: string } = await data.json()
